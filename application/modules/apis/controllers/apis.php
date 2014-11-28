c65475f92ed411a776bdb08935a0284895e3c4fc
rohmanahmad <rohmanmail@gmail.com>
2014-11-28 09:27:33 +0700

    adding apis

---
+++ application/modules/apis/controllers/apis.php
@@ -0,0 +1,18 @@
+<?php
+class Apis extends CI_Controller{
+
+ function __construct(){
+ 	parent::__construct();
+ 	$this->load->model("apis_model","m");
+ }
+ 
+ function index(){
+ }
+ 
+ function inputInbox($phone,$key,$filter=""){
+ 	
+ }
+
+
+}
+?>
